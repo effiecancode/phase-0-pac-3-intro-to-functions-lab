4946be8915535283ac81a358aadf5d45b8aaebdc
effiecancode <faitheffie25@gmail.com>
2023-12-02 12:37:06 +0300

    all tests passed 🤩

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(string.toLowerCase());
 }
+
+function sayHiToHeadphonedRoommate(string) {
+    if(string === string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+
+    } else if (string === 'Let\'s have dinner together!') {
+        return "I would love to!";
+    }
+}
